d9df20f8baf76939b0effb455b98af5a9f1f7f73
Minh Doan <minh.doan04@gmail.com>
2017-07-10 14:47:10 +0700

    Implemented Dynamic Array

---
+++ src/ds/arrays/DynamicArray.java
@@ -0,0 +1,133 @@
+package ds.arrays;
+
+import java.util.Arrays;
+
+public class DynamicArray<T> {
+	private int size;
+	private int capacity;
+	private Object[] elementList;
+	private static final int MIN_CAPACITY = 16;
+	
+	public DynamicArray() {
+		this(16);
+	}
+	
+	public DynamicArray(int initialCapacity) {
+		if (initialCapacity < 16) {
+			capacity = 16;
+		} else {
+			capacity = initialCapacity;
+		}
+		size = 0;
+		elementList = new Object[capacity];
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public int capacity() {
+		return capacity;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T at(int index) {
+		checkIndex(index);
+		return (T)elementList[index];
+	}
+	
+	public void push(T item) {
+		checkForResize(size+1);
+		elementList[size] = item;
+		size++;
+	}
+	
+	public void insert(int index, T item) {
+		checkIndex(index);
+		checkForResize(size+1);
+		System.arraycopy(elementList, index, elementList, index + 1, size - index);
+		elementList[index] = item;
+		size++;
+ 	}
+	
+	public void prepend(T item) {
+		insert(0, item);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T pop() {
+		if (size == 0) {
+			throw new Error("There is no item in the array");
+		}
+		T tempItem = (T)elementList[size-1];
+		elementList[size-1] = null;
+		size--;
+		checkForResize(size);
+		return tempItem;
+	}
+	
+	public void delete(int index) {
+		checkIndex(index);
+		if (index == size-1) {
+			pop();
+			return;
+		} else {
+			System.arraycopy(elementList, index + 1, elementList, index, size - index);
+		}
+		elementList[size-1] = null;
+		this.size--;
+		checkForResize(size);
+	}
+	
+	public void remove(T item) {
+		for (int i = 0; i < size; i++) {
+			if (elementList[i] == item) {
+				delete(i);
+				i--;
+			}
+		}
+	}
+	
+	public int find(T item) {
+		for (int i = 0; i < size; i++) {
+			if (item.equals(elementList[i])) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	private void checkForResize(int size) {
+		if (size < capacity && size > capacity/4) {
+			return;
+		}
+		if (size <= capacity/4) {
+			if (capacity == MIN_CAPACITY) {
+				return;
+			}
+			if (capacity / 2 < MIN_CAPACITY) {
+				resize(MIN_CAPACITY);
+			} else {
+				resize(capacity/2);
+			}
+		}
+		if (size >= capacity) {
+			resize(capacity*2);
+		}
+	}
+	
+	private void resize(int newCapacity) {
+		capacity = newCapacity;
+		elementList = Arrays.copyOf(elementList, newCapacity);
+	}
+	
+	private void checkIndex(int index) {
+		if (index < 0 || index > size) {
+			throw new IndexOutOfBoundsException("Index is either less than 0 or great ther than size: " + this.size);
+		}
+	}
+}
---
+++ src/ds/arrays/DynamicArrayTest.java
@@ -0,0 +1,132 @@
+package ds.arrays;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DynamicArrayTest {
+
+	@Test
+	public void testConstructor() {
+		DynamicArray<Integer> testArray = new DynamicArray<Integer>();
+		assertEquals(testArray.capacity(), 16);
+		DynamicArray<Integer> testArray1 = new DynamicArray<Integer>(20);
+		assertEquals(testArray1.capacity(), 20);
+	}
+	
+	@Test
+	public void testPush() {
+		DynamicArray<String> testArray = new DynamicArray<String>();
+		testArray.push("abc");
+		testArray.push("def");
+		testArray.push("ghi");
+		testArray.push("jkl");
+		testArray.push("mno");
+		assertEquals(testArray.size(), 5);
+		assertEquals(testArray.at(0), "abc");
+	}
+	
+	@Test
+	public void testPushOverCapacity() {
+		DynamicArray<Integer> testArray = new DynamicArray<Integer>();
+		int initialCapacity = testArray.capacity();
+		for (int i = 0; i < initialCapacity; i++) {
+			testArray.push(i);
+		}
+		testArray.push(initialCapacity + 1);
+		assertEquals(testArray.size(), 17);
+		assertEquals(testArray.capacity(), 32);
+		assertEquals(testArray.at(16), new Integer(17));
+	}
+	
+	@Test
+	public void testInsert() {
+		DynamicArray<Integer> testArray = new DynamicArray<Integer>();
+		for (int i = 0; i < 5; i++) {
+			testArray.push(i);
+		}
+		testArray.insert(2, 3);
+		assertEquals(testArray.size(), 6);
+		assertEquals(testArray.capacity(), 16);
+		assertTrue(testArray.at(2) == 3);
+		assertTrue(testArray.at(3) == 2);
+		assertTrue(testArray.at(5) == 4);
+	}
+	
+	@Test
+	public void testPrepend() {
+		DynamicArray<String> testArray = new DynamicArray<String>();
+		testArray.push("Array");
+		testArray.push("List");
+		testArray.push("Apple");
+		testArray.prepend("Orange");
+		assertEquals(testArray.at(0), "Orange");
+		assertEquals(testArray.size(), 4);
+	}
+
+	@Test(expected = Error.class)
+	public void testPopEmptyArray() {
+		DynamicArray<String> testArray = new DynamicArray<String>();
+		testArray.pop();
+	}
+	
+	@Test
+	public void testPop() {
+		DynamicArray<String> testArray = new DynamicArray<String>();
+		testArray.push("Array");
+		testArray.push("List");
+		testArray.push("Apple");
+		String apple = testArray.pop();
+		assertEquals(apple, "Apple");
+		assertEquals(testArray.at(1), "List");
+		assertEquals(testArray.capacity(), 16);
+		assertEquals(testArray.size(), 2);
+	}
+	
+	@Test
+	public void testDelete() {
+		DynamicArray<Integer> testArray = new DynamicArray<Integer>();
+		for (int i = 0; i < 31; i++) {
+			testArray.push(i);
+		}
+		for (int i = 0; i < 24; i++) {
+			testArray.delete(7);
+		}
+		assertEquals(testArray.size(), 7);
+		assertEquals(testArray.capacity(), 16);
+		assertTrue(testArray.at(3) == 3);
+	}
+	
+	@Test
+	public void testRemove() {
+		DynamicArray<String> testArray = new DynamicArray<String>();
+		testArray.push("Array");
+		testArray.push("List");
+		testArray.push("Apple");
+		testArray.push("List");
+		testArray.push("Lemon");
+		testArray.push("Pingpong");
+		testArray.push("List");
+		testArray.remove("List");
+		assertEquals(testArray.size(), 4);
+		assertEquals(testArray.at(1), "Apple");
+		assertEquals(testArray.pop(), "Pingpong");
+	}
+	
+	@Test
+	public void testFind() {
+		DynamicArray<String> testArray = new DynamicArray<String>();
+		testArray.push("Array");
+		testArray.push("List");
+		testArray.push("Apple");
+		testArray.push("List");
+		testArray.push("Lemon");
+		testArray.push("Pingpong");
+		testArray.push("List");
+		assertEquals(testArray.find("List"), 1);
+		testArray.delete(1);;
+		assertEquals(testArray.find("List"), 2);
+		testArray.remove("List");
+		assertEquals(testArray.find("List"), -1);
+	}
+}
